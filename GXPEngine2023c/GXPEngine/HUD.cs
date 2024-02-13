8d46502a1be1faf367e31522519fabf297b34823
Fibeer1 <fibeer67@gmail.com>
2024-02-13 12:31:40 +0100

    updated scripts

---
+++ GXPEngine2023c/GXPEngine/HUD.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GXPEngine
+{
+    class HUD : EasyDraw
+    {
+        Player player;
+        public HUD() : base(800, 600, false) //size is the same as the game window
+        {
+            Start();
+        }
+        void Start()
+        {
+            player = game.FindObjectOfType(typeof(Player)) as Player;
+        }
+    }
+}
