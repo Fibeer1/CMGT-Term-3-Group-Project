b9910c2afa28311e53e46f9abe6c1824513676bb
JorisKalk <151745455+JorisKalk@users.noreply.github.com>
2024-02-15 11:52:18 +0100

    data
    added data to EnemyData and added HUDData

--- GXPEngine2023c/GXPEngine/EnemyData.cs
+++ GXPEngine2023c/GXPEngine/EnemyData.cs
@@ -8,7 +8,71 @@ namespace GXPEngine
 {
     public class EnemyData
     {
+        const float normalMarshmallowSpeed = 0;
+        const float normalMarshmallowJumpHeight = 15;
+        const float normalMarshmallowDamage = 20;
 
+        const float burningMarshmallowSpeed = 5;
+        const float burningMarshmallowJumpHeight = 0;
+        const float burningMarshmallowDamage = 40;
+
+        const float shooterMarshmallowDamage = 80;
+
+        public float normalSpeed
+        {
+            get
+            {
+                return normalMarshmallowSpeed;
+            }
+        }
+
+        public float normalJumpHeight
+        {
+            get
+            {
+                return normalMarshmallowJumpHeight;
+            }
+        }
+
+        public float normalDamage
+        {
+            get
+            {
+                return normalMarshmallowDamage;
+            }
+        }
+
+        public float burningSpeed
+        {
+            get
+            {
+                return burningMarshmallowSpeed;
+            }
+        }
+
+        public float burningJumpHeight
+        {
+            get
+            {
+                return burningMarshmallowJumpHeight;
+            }
+        }
+
+        public float burningDamage
+        {
+            get
+            {
+                return burningMarshmallowDamage;
+            }
+        }
+
+        public float shooterDamage
+        {
+            get
+            {
+                return shooterMarshmallowDamage;
+            }
+        }
 
         public EnemyData()
         {
